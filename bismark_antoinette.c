6341620924c5504b901934e63a22d7f7c8ce8735
BismarkAidoo <mc07.aidoo@gmail.com>
2023-05-12 09:11:07 +0000

    A beautiful code

---
+++ bismark_antoinette.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char a[] = "Antoinette";
+	char b[] = "Bismark";
+
+	printf("%s and %s are the authors of this repository\n", a, b);
+
+	return (0);
+}
