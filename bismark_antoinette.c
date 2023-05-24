35f3b9c7a641684186ead5bd98f3fcdc97167eaf
BismarkAidoo <mc07.aidoo@gmail.com>
2023-05-24 21:39:02 +0000

    Keeping it simple

--- bismark_antoinette.c
+++ bismark_antoinette.c
@@ -1,36 +1,14 @@
 #include "shell.h"
 
 /**
- * add - a function that adds two numbers
+ * Sum - a function that adds two numbers
  * @a: first parameter
  * @b: second parameter
  *
- * Return: Sum of the two numbers
+ * Return: Always (0) success
  */
 
 int sum(int a, int b)
 {
-	return (a + b);
+	return ((a) + (b));
 }
-
-/**
- * main - Entry point of the program
- *
- * Return: Always (0) success
- *
-
-int main(void)
-{
-	int num1 = 3;
-	int num2 = 7;
-	int result;
-
-	result = add(num1, num2);
-
-	char buffer[100];
-	int length = snprintf(buffer, sizeof(buffer),
-			"The sum of %d and %d is: %d\n", num1, num2, results);
-	write(1, butter, length);
-
-	return (0);
-}*/
